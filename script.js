0ec55ab3dcfbad7c09bf6aad1153ac230ee35ad3
Neginadg <abdolbaghinegin@gmail.com>
2025-07-18 17:37:17 +0300

    open, close and overlay are working

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnOpenModal = document.querySelectorAll('.show-modal');
 const btnCloseModal = document.querySelector('.close-modal');
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', () => {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+  });
+}
+
+btnCloseModal.addEventListener('click', () => {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
